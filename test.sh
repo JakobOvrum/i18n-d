7a5c64086faab3d848f8f0afe440ea1b963d9c6d
Jakob Øvrum <jakobovrum@gmail.com>
2016-04-04 18:58:37 +0900

    Add empty test

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
 #/bin/sh
-LANGUAGE=ja_JP.UTF8:de_DE.UTF-8 dub test
+
+# empty
+LANGUAGE=ja_JP.UTF8 dub test --config="test-empty"
+LANGUAGE=en dub test --config="test-empty"
+LANGUAGE="" dub test --config="test-empty"
+
+# ja-de
+LANGUAGE=ja_JP.UTF8:de_DE.UTF-8 dub test --config="test-ja-de"
+# fr and es are not provided, so they should be ignored
+LANGUAGE=fr_FR@euro:ja_JP:es:de_DE.UTF-8 dub test --config="test-ja-de"
 
